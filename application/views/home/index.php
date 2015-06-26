462198575290b7ea98fdfda41bcc3e976bf99e41
ginovski <sashko.ginovski@gmail.com>
2015-06-26 20:42:51 +0300

    Add home controller and view home/index

---
+++ application/views/home/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Name - Personal Blog</title>
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+
+    <script src="/assets/scripts/bootstrap.min.js"></script>
+    <script src="/assets/scripts/jquery-2.1.4.min.js"></script>
+    <script src="/assets/scripts/less.min.js"></script>
+    <script src="/assets/scripts/comments-color-change.js"></script>
+    <script src="/assets/scripts/progress-bar-width.js"></script>
+    <script src="/assets/scripts/smooth-scroll.js"></script>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Name <small>Moto or profession</small></a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active nav-link"><a href="#">Home</a></li>
+                    <li class="nav-link"><a href="#">Portfolio</a></li>
+                    <li class="nav-link"><a href="#">Newsletter</a></li>
+                    <li class="nav-link"><a href="#">About</a></li>
+                    <li class="nav-link"><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
