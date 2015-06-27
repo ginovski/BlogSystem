d536d163eecf3918c6afb986353ec92391d6a0a1
KrasiStoyanov <kradi_st@abv.bg>
2015-06-27 17:33:39 +0300

    Add example post in home page

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,1 +1,43 @@
-<h1>Posts</h1>
+<div id="main-content">
+    <div class="articles">
+        <div class="content-body article image-article">
+            <div class="image">
+                <img src="assets/images/testImage.jpg" alt="Test Image">
+            </div>
+
+            <div class="main-body">
+                <div class="title">
+                    <h2>Standart Post With a Featured Image</h2>
+                </div>
+
+                <div class="date-comments">
+                    <div class="date">
+                        <p><i class="fa fa-clock-o"></i> 15 May, 2015</p>
+                    </div>
+
+                    <div class="comments">
+                        <p>
+                            <a href="post-without-comments.html">
+                                <i class="fa fa-comments"></i>
+                                <span class="number-comments">0</span> comments
+                            </a>
+                        </p>
+                    </div>
+
+                    <div class="like">
+                        <a href=""><i class="like fa fa-heart"></i> Like</a>
+                    </div>
+                </div>
+
+                <div class="post-text">
+                    <p class="light-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget aliquet ante. Ut blandit bibendum sapien sed condimentum. Sed sit amet justo a quam dapibus semper. Duis sit amet vulputate sapien. Nullam quis nisl dolor. Sed gravida ullamcorper nulla sed rutrum. Nullam vel nisi finibus, luctus velit iaculis, ultricies eros. Sed dapibus, elit in fermentum pulvinar, ligula dui interdum tortor, ac pellentesque lacus lacus tincidunt velit.</p>
+                    <p class="dark-text">In rhoncus vitae ligula non dignissim. Cras mattis augue quis tempus facilisis. Donec finibus leo consequat auctor rhoncus. Vivamus volutpat aliquet lectus nec condimentum. Proin non purus facilisis est egestas aliquet. Phasellus pretium arcu non pellentesque facilisis. Donec finibus diam sit amet tincidunt auctor. Cras in nunc non elit luctus tempus in sed sapien. Mauris at dignissim ex, a varius enim. Aenean gravida, nisl aliquam ultrices pellentesque, quam augue gravida eros, id aliquam dolor est sagittis dolor. Nullam dui quam, semper vel tincidunt porttitor, porttitor ac ligula. Aenean pellentesque nisi vitae urna volutpat volutpat. In pulvinar diam id dolor dictum dignissim.</p>
+                </div>
+
+                <div class="read-more">
+                    <a href="post-without-comments.html">read more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
