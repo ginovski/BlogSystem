df24fbc4fc25832c1eb8d839e68f3f30898797ce
KrasiStoyanov <kradi_st@abv.bg>
2015-06-27 16:49:27 +0300

    Add contact page

---
+++ application/views/home/contact.php
@@ -0,0 +1,25 @@
+<div id="main-content">
+    <div class="content-body contact">
+        <div class="main-body">
+            <div class="title">
+                <h2>Get In Touch</h2>
+                <div class="name">
+                    <h2 style="color: #54c0b0;">
+                        [Name]
+                        <small><i class="fa fa-phone"></i> [Phone Number]</small>
+                        <small><i class="fa fa-envelope"></i> [Email]</small>
+                    </h2>
+                </div>
+            </div>
+
+            <div class="form">
+                <form action="">
+                    <input class="input" type="text" placeholder="Name*">
+                    <input class="input" type="email" placeholder="Email*">
+                    <textarea class="textarea input" name="Message" placeholder="Message*"></textarea>
+                    <input class="submit" type="submit" value="send">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
