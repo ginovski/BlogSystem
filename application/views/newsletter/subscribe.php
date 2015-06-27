4b243aad200c62bff19daa79ea0657620cb9d1d1
KrasiStoyanov <kradi_st@abv.bg>
2015-06-27 17:03:24 +0300

    Finish the subscribe page

---
+++ application/views/newsletter/subscribe.php
@@ -0,0 +1,18 @@
+<div id="main-content">
+    <div class="content-body newsletter">
+        <div class="main-body">
+            <div class="title">
+                <h2>Newsletter</h2>
+                <small>Want to recieve all the latest news? If so, then subscribe below.</small>
+            </div>
+
+            <div class="form">
+                <form action="">
+                    <input class="input" type="email" placeholder="Email*">
+                    <div class="col-sm-5"></div>
+                    <input class="submit" type="submit" value="subscribe">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
