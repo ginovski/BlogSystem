e387e6a96cfe48a57de65f29121425d2f6ba4fa1
KrasiStoyanov <kradi_st@abv.bg>
2015-06-27 21:12:34 +0300

    Slightly changed the design of the articles

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,50 +1,47 @@
 <div id="main-content">
     <div class="articles">
-            <?php foreach($posts as $post):?>
-            <?php if(!is_null($post['imageURL'])):?>
-                <div class="content-body article image-article">
+        <?php foreach($posts as $post):?>
+        <div class="content-body article image-article">
+            <div class="main-body">
+                <div class="title">
+                    <h2><?php echo $post['title']?></h2>
+                </div>
+
+                <div class="date-comments">
+                    <div class="author">
+                        <p><i class="fa fa-user"></i> <?php echo $post['author']?></p>
+                    </div>
+
+                    <div class="date">
+                        <p><i class="fa fa-clock-o"></i> <?php echo $post['date']?></p>
+                    </div>
+
+                    <div class="comments">
+                        <p>
+                            <a href="<?php echo base_url(). 'index.php/posts/details/'.$post['id']?>">
+                                <i class="fa fa-comments"></i>
+                                <span class="number-comments"><?php echo $comments[$post['id']]?></span> comments
+                            </a>
+                        </p>
+                    </div>
+
+                    <div class="like">
+                        <a href=""><i class="like fa fa-heart"></i> <?php echo $post['likes']?></a>
+                    </div>
+                </div>
+
                 <div class="image">
                     <img src="<?php echo base_url(). $post['imageURL']?>" alt="Test Image">
                 </div>
-            <?php else: ?>
-                <div class="content-body text-article">
-            <?php endif;?>
-                <div class="main-body">
-                    <div class="title">
-                        <h2><?php echo $post['title']?></h2>
-                    </div>
 
-                    <div class="date-comments">
-                        <div class="author">
-                            <p><i class="fa fa-user"></i> <?php echo $post['author']?></p>
-                        </div>
-
-                        <div class="date">
-                            <p><i class="fa fa-clock-o"></i> <?php echo $post['date']?></p>
-                        </div>
-
-                        <div class="comments">
-                            <p>
-                                <a href="<?php echo base_url(). 'index.php/posts/details/'.$post['id']?>">
-                                    <i class="fa fa-comments"></i>
-                                    <span class="number-comments"><?php echo $comments[$post['id']]?></span> comments
-                                </a>
-                            </p>
-                        </div>
-
-                        <div class="like">
-                            <a href=""><i class="like fa fa-heart"></i> <?php echo $post['likes']?></a>
-                        </div>
-                    </div>
-
-                    <div class="post-text">
-                        <p class="dark-text"><?php echo $post['content']?></p>
-                    </div>
-
-                    <div class="read-more">
-                        <a href="<?php echo base_url(). 'index.php/posts/details/'.$post['id']?>">read more</a>
-                    </div>
+                <div class="post-text">
+                    <p class="dark-text"><?php echo $post['content']?></p>
                 </div>
+
+                <div class="read-more">
+                    <a href="<?php echo base_url(). 'index.php/posts/details/'.$post['id']?>">read more</a>
+                </div>
+            </div>
         </div>
     <?php endforeach;?>
     </div>
