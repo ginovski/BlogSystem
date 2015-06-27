1aaafcec365b9876b59437afb862328b9b450135
KrasiStoyanov <kradi_st@abv.bg>
2015-06-27 16:55:57 +0300

    Finish the design for the contact page

--- application/views/home/contact.php
+++ application/views/home/contact.php
@@ -5,8 +5,9 @@
                 <h2>Get In Touch</h2>
                 <div class="name">
                     <h2 style="color: #54c0b0;">
-                        <?php echo $user_info['fullname']?>
+                        <?php echo $user_info['fullname']?> <br/>
                         <small><i class="fa fa-phone"></i> <?php echo $user_info['phone']?></small>
+                        <br/>
                         <small><i class="fa fa-envelope"></i> <?php echo $user_info['email']?></small>
                     </h2>
                 </div>
