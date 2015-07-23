37c16586ba75599de19ce985a7c18e07ee21689c
KrasiStoyanov <kradi_st@abv.bg>
2015-07-23 17:05:59 +0300

    Added some smooth animations for the hamburger collapsing menu

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -22,16 +22,16 @@
     <nav class="navbar navbar-default">
         <div class="container-fluid">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                     <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
+                    <span class="icon-bar top-bar"></span>
+                    <span class="icon-bar middle-bar"></span>
+                    <span class="icon-bar bottom-bar"></span>
                 </button>
                 <a class="navbar-brand" href="<?php echo base_url(). 'home'?>"><?php echo $user_info['fullname']?> <small><?php echo $user_info['profession']?></small></a>
             </div>
 
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="collapse navbar-collapse" id="navbar">
                 <ul class="nav navbar-nav navbar-right">
                     <li class="nav-link"><a href="<?php echo base_url(). 'home'?>">Home</a></li>
                     <li class="nav-link"><a href="#">Portfolio</a></li>
