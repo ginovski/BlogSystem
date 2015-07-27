27c1c4c4bff402be5a356af7761ece032d01c003
KrasiStoyanov <kradi_st@abv.bg>
2015-07-27 12:53:38 +0300

    Home section in admin panel is ready

--- application/views/administration/home/index.php
+++ application/views/administration/home/index.php
@@ -1,5 +1,40 @@
     <div id="main-content">
-        <div class="container">
+        <div class="container home">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="panel panel-stats panel-blue">
+                        <div class="panel-heading text-center">
+                            <i class="glyphicon glyphicon-align-left"></i>
+                            <h3>140</h3>
+                        </div>
+                        <div class="panel-body text-center">
+                            <a href="<?php echo base_url().'administration/posts/index'?>">Posts</a>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="col-md-4">
+                    <div class="panel panel-stats panel-green">
+                        <div class="panel-heading text-center">
+                            <i class="glyphicon glyphicon-comment"></i>
+                            <h3>500 000</h3>
+                        </div>
+                        <div class="panel-body text-center">
+                            <a href="<?php echo base_url().'administration/comments/index'?>">Comments</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="panel panel-stats panel-red">
+                        <div class="panel-heading text-center">
+                            <i class="glyphicon glyphicon-user"></i>
+                        </div>
+                        <div class="panel-body text-center">
+                            <a href="<?php echo base_url().'administration/about/index'?>">About</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
