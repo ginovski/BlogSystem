e6593de52e61c7d2934275b1ab835f00080c5631
KrasiStoyanov <kradi_st@abv.bg>
2015-08-13 18:31:26 +0300

    Created sample layout for adding a post in administration section.

--- application/views/administration/posts/add.php
+++ application/views/administration/posts/add.php
@@ -1,41 +1,59 @@
 <div id="main-content">
-    <div class="articles detail-view">
+    <div class="articles detail-view add-post">
         <div class="content-body text-article">
             <div class="main-body">
-<!--                <div class="image">-->
-<!--                    <img src="--><?php //echo base_url(). $post['imageURL']?><!--" alt="Test Image">-->
-<!--                </div>-->
+                <form action="">
+                    <div class="image">
+                        <div class="wrapper">
+                            <div class="opacity-overlay">
+                                <p>
+                                    <a href=""><i class="glyphicon glyphicon-pencil"></i></a>
+                                    <a href=""><i class="glyphicon glyphicon-remove"></i></a>
+                                </p>
+                            </div>
 
-                <div class="title">
-                    <h2>[Post title]</h2>
-                </div>
+                            <img src="http://static.comicvine.com/uploads/original/11111/111111753/2950477-gotham1.jpg" alt="Test Image">
+                        </div>
 
-                <div class="date-comments">
-                    <div class="author">
-                        <p><i class="fa fa-user"></i> Me</p>
+                        <a class="image-placeholder">
+                            <p><i class="glyphicon glyphicon-plus"></i></p>
+                            <span>Upload image</span>
+                        </a>
                     </div>
 
-                    <div class="date">
-                        <p><i class="fa fa-clock-o"></i> yesterday</p>
+                    <div class="title">
+<!--                        <h2>[Post title]</h2>-->
+                        <input class="input" type="text" name="post-title" required="required" value="" placeholder="Post title">
                     </div>
 
-                    <div class="comments">
-                        <p>
-                            <a href="#comments">
-                                <i class="fa fa-comments"></i>
-                                <span class="number-comments">2</span> comments
-                            </a>
-                        </p>
-                    </div>
+<!--                    <div class="date-comments">-->
+<!--                        <div class="author">-->
+<!--                            <p><i class="fa fa-user"></i> Me</p>-->
+<!--                        </div>-->
+<!---->
+<!--                        <div class="date">-->
+<!--                            <p><i class="fa fa-clock-o"></i> yesterday</p>-->
+<!--                        </div>-->
+<!---->
+<!--                        <div class="comments">-->
+<!--                            <p>-->
+<!--                                <a href="#comments">-->
+<!--                                    <i class="fa fa-comments"></i>-->
+<!--                                    <span class="number-comments">2</span> comments-->
+<!--                                </a>-->
+<!--                            </p>-->
+<!--                        </div>-->
+<!---->
+<!--                        <div class="like">-->
+<!--                            <a href=""><i class="like fa fa-heart"></i> 50000</a>-->
+<!--                        </div>-->
+<!--                    </div>-->
 
-                    <div class="like">
-                        <a href=""><i class="like fa fa-heart"></i> 50000</a>
+                    <div class="post-text">
+                        <p class="dark-text"></p>
+                        <textarea class="textarea input" name="pos-text" required="required" placeholder="Post text"></textarea>
                     </div>
-                </div>
-
-                <div class="post-text">
-                    <p class="dark-text">sssssssssssssssssssssssssssssss</p>
-                </div>
+                </form>
             </div>
         </div>
     </div>
