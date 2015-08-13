62765ea7a5688f75cf7645b0af84f96718840ae7
KrasiStoyanov <kradi_st@abv.bg>
2015-08-13 18:43:19 +0300

    Done with the sample layout for adding post page.

--- application/views/administration/posts/add.php
+++ application/views/administration/posts/add.php
@@ -6,10 +6,10 @@
                     <div class="image">
                         <div class="wrapper">
                             <div class="opacity-overlay">
-                                <p>
-                                    <a href=""><i class="glyphicon glyphicon-pencil"></i></a>
-                                    <a href=""><i class="glyphicon glyphicon-remove"></i></a>
-                                </p>
+                                <div>
+                                    <a title="Change" class="change-image" href=""><i class="glyphicon glyphicon-pencil"></i></a>
+                                    <a title="Remove" class="remove-image" href=""><i class="glyphicon glyphicon-remove"></i></a>
+                                </div>
                             </div>
 
                             <img src="http://static.comicvine.com/uploads/original/11111/111111753/2950477-gotham1.jpg" alt="Test Image">
@@ -22,35 +22,10 @@
                     </div>
 
                     <div class="title">
-<!--                        <h2>[Post title]</h2>-->
                         <input class="input" type="text" name="post-title" required="required" value="" placeholder="Post title">
                     </div>
 
-<!--                    <div class="date-comments">-->
-<!--                        <div class="author">-->
-<!--                            <p><i class="fa fa-user"></i> Me</p>-->
-<!--                        </div>-->
-<!---->
-<!--                        <div class="date">-->
-<!--                            <p><i class="fa fa-clock-o"></i> yesterday</p>-->
-<!--                        </div>-->
-<!---->
-<!--                        <div class="comments">-->
-<!--                            <p>-->
-<!--                                <a href="#comments">-->
-<!--                                    <i class="fa fa-comments"></i>-->
-<!--                                    <span class="number-comments">2</span> comments-->
-<!--                                </a>-->
-<!--                            </p>-->
-<!--                        </div>-->
-<!---->
-<!--                        <div class="like">-->
-<!--                            <a href=""><i class="like fa fa-heart"></i> 50000</a>-->
-<!--                        </div>-->
-<!--                    </div>-->
-
                     <div class="post-text">
-                        <p class="dark-text"></p>
                         <textarea class="textarea input" name="pos-text" required="required" placeholder="Post text"></textarea>
                     </div>
                 </form>
