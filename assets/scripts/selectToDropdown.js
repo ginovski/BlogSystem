de5e892b507c4fbe670b7e0a7990afc90ccd8621
KrasiStoyanov <kradi_st@abv.bg>
2015-08-14 21:31:28 +0300

    Created a dropdown menu for selecting a category in the add post page in the administration section. (closes #25)

--- assets/scripts/selectToDropdown.js
+++ assets/scripts/selectToDropdown.js
@@ -2,16 +2,57 @@
 
 $(document).ready(function () {
     function createDropdownElement($selector) {
-        var $categorySelect = $('#category'),
+        var i, len,
+            $categorySelect = $('#category'),
             $selectChildren = $categorySelect.children(),
-            $categoryDropdownDiv = $('<div/>');
+            $categoryDropdownDiv = $('<div/>'),
+            $toggleButton = $('<button/>'),
+            $caret = $('<span/>'),
+            $dropdownUl = $('<ul/>');
 
         $categorySelect.addClass('hidden');
+        $categoryDropdownDiv.addClass('dropdown')
+            .css('display', 'inline-block');
 
-        $categoryDropdownDiv.addClass('dropdown');
+        $toggleButton.addClass('btn')
+            .addClass('btn-default')
+            .addClass('dropdown-toggle')
+            .attr('type', 'button')
+            .attr('id', 'category-select-dropdown')
+            .attr('data-toggle', 'dropdown')
+            .attr('aria-haspopup', 'true')
+            .attr('aria-expanded', 'true')
+            .html('Select a category ');
 
+        $caret.addClass('caret');
+        $caret.appendTo($toggleButton);
 
+        $dropdownUl.addClass('dropdown-menu')
+            .attr('aria-labelledby', 'category-select-dropdown');
+
+        for (i = 0, len = $selectChildren.length; i < len; i += 1) {
+            var $currentOption = $($selectChildren[i]),
+                $newListItem = $('<li/>'),
+                $newAnchorTag = $('<a/>');
+
+            $newAnchorTag.attr('href', '#')
+                .attr('data-value', $currentOption.val())
+                .html($currentOption.html())
+                .css('text-align', 'center');
+
+            $newAnchorTag.appendTo($newListItem);
+            $newListItem.appendTo($dropdownUl);
+        }
+
+        $toggleButton.appendTo($categoryDropdownDiv);
+        $dropdownUl.appendTo($categoryDropdownDiv);
         $categoryDropdownDiv.appendTo($selector);
+
+        $('.dropdown-menu a').on('click', function () {
+            var $this = $(this),
+                $dataValue = $this.attr('data-value');
+            $categorySelect.val($dataValue);
+        });
     }
 
     createDropdownElement($('.category'));
