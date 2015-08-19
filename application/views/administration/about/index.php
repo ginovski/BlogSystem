2c549e4fb76ca8e277260ca7815fe9ccb5a879dc
KrasiStoyanov <kradi_st@abv.bg>
2015-08-19 20:36:38 +0300

    Added some basic edit in the about page in the administration section.

---
+++ application/views/administration/about/index.php
@@ -0,0 +1,270 @@
+<div id="wrapper"><div id="main-content">
+        <div class="content-body about">
+            <div class="main-body">
+                <form action="">
+                    <div class="name">
+                        <input class="edit-input full-name" name="full-name" value="Aleksandar Ginovski" type="text"/>
+                        <input class="edit-input small" name="work-position" value="Web Developer" type="text"/>
+                    </div>
+
+                    <div class="about-text">
+                        <textarea class="edit-input" name="about-text" id="about-text">I am a young, creative and broad-minded front-end developer, with great communication and teamwork skills. A lot of experience in the sphere of web and graphic design. I am very hardworking and have the intention to broaden my knowledge in the sphere of web programming. I aim at becomming a professional Front-End Developer.</textarea>
+                    </div>
+
+                    <div id="skills">
+                        <div class="skills-title">
+                            <h2>Skills</h2>
+                        </div>
+
+                        <div class="skills row">
+                            <div class="col-sm-6 col-xs-12 professional">
+                                <small>Professional</small>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-8 col-xs-12 skill-title">
+                                        <input class="edit-input small" name="skill-name" value="C#" type="text"/>
+                                    </div>
+
+                                    <div class="col-sm-4 col-xs-12">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="number"/>
+                                        <div class="progress-bar hidden hidden" data-toggle="tooltip" title="" data-original-title="80%">
+                                            <div class="green-bar" data-width="80" style="width: 80%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-8 col-xs-12 skill-title">
+                                        <input class="edit-input small" name="skill-name" value="JavaScript" type="text"/>
+                                    </div>
+
+                                    <div class="col-sm-4 col-xs-12">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="number"/>
+                                        <div class="progress-bar hidden" data-toggle="tooltip" title="" data-original-title="80%">
+                                            <div class="green-bar" data-width="80" style="width: 80%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-8 col-xs-12 skill-title">
+                                        <input class="edit-input small" name="skill-name" value="PHP" type="text"/>
+                                    </div>
+
+                                    <div class="col-sm-4 col-xs-12">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="number"/>
+                                        <div class="progress-bar hidden" data-toggle="tooltip" title="" data-original-title="80%">
+                                            <div class="green-bar" data-width="80" style="width: 80%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-8 col-xs-12 skill-title">
+                                        <input class="edit-input small" name="skill-name" value="ASP.NET" type="text"/>
+                                    </div>
+
+                                    <div class="col-sm-4 col-xs-12">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="number"/>
+                                        <div class="progress-bar hidden" data-toggle="tooltip" title="" data-original-title="80%">
+                                            <div class="green-bar" data-width="80" style="width: 80%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-8 col-xs-12 skill-title">
+                                        <input class="edit-input small" name="skill-name" value="MS Office" type="text"/>
+                                    </div>
+
+                                    <div class="col-sm-4 col-xs-12">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="number"/>
+                                        <div class="progress-bar hidden" data-toggle="tooltip" title="" data-original-title="70%">
+                                            <div class="green-bar" data-width="70" style="width: 70%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-8 col-xs-12 skill-title">
+                                        <input class="edit-input small" name="skill-name" value="C++" type="text"/>
+                                    </div>
+
+                                    <div class="col-sm-4 col-xs-12">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="number"/>
+                                        <div class="progress-bar hidden" data-toggle="tooltip" title="" data-original-title="50%">
+                                            <div class="green-bar" data-width="50" style="width: 50%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-8 col-xs-12 skill-title">
+                                        <input class="edit-input small" name="skill-name" value="Java" type="text"/>
+                                    </div>
+
+                                    <div class="col-sm-4 col-xs-12">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="number"/>
+                                        <div class="progress-bar hidden" data-toggle="tooltip" title="" data-original-title="40%">
+                                            <div class="green-bar" data-width="40" style="width: 40%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+    
+                            <div class="col-sm-6 col-xs-12 personal">
+                                <small>Personal</small>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-8 col-xs-12 skill-title">
+                                        <input class="edit-input small" name="skill-name" value="Communication" type="text"/>
+                                    </div>
+
+                                    <div class="col-sm-4 col-xs-12">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="number"/>
+                                        <div class="progress-bar hidden" data-toggle="tooltip" title="" data-original-title="80%">
+                                            <div class="green-bar" data-width="80" style="width: 80%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-8 col-xs-12 skill-title">
+                                        <input class="edit-input small" name="skill-name" value="Creativity" type="text"/>
+                                    </div>
+
+                                    <div class="col-sm-4 col-xs-12">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="number"/>
+                                        <div class="progress-bar hidden" data-toggle="tooltip" title="" data-original-title="70%">
+                                            <div class="green-bar" data-width="70" style="width: 70%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-8 col-xs-12 skill-title">
+                                        <input class="edit-input small" name="skill-name" value="Teamwork" type="text"/>
+                                    </div>
+
+                                    <div class="col-sm-4 col-xs-12">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="number"/>
+                                        <div class="progress-bar hidden" data-toggle="tooltip" title="" data-original-title="60%">
+                                            <div class="green-bar" data-width="60" style="width: 60%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-8 col-xs-12 skill-title">
+                                        <input class="edit-input small" name="skill-name" value="Leadership" type="text"/>
+                                    </div>
+
+                                    <div class="col-sm-4 col-xs-12">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="number"/>
+                                        <div class="progress-bar hidden" data-toggle="tooltip" title="" data-original-title="60%">
+                                            <div class="green-bar" data-width="60" style="width: 60%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="col-sm-8 col-xs-12 skill-title">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="text"/>
+                                    </div>
+
+                                    <div class="col-sm-4 col-xs-12">
+                                        <input class="edit-input small" name="skill-name" value="Organisation" type="number"/>
+                                        <div class="progress-bar hidden" data-toggle="tooltip" title="" data-original-title="50%">
+                                            <div class="green-bar" data-width="50" style="width: 50%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="skills">
+                        <div class="skills-title">
+                            <h2>Projects</h2>
+                        </div>
+
+                        <div class="portfolio row">
+                            <div class="col-md-12">
+                                <div class="portfolio-data">
+                                    <a href="">[Project Title]</a>
+                                    <small>(date)</small>
+
+                                    <div>
+                                        <small>Description: </small>
+                                        <span>[description]</span>
+                                    </div>
+
+                                    <div>
+                                        <small>Technologies: </small>
+                                        <span>[technology], [technology]</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="skills">
+                        <div class="skills-title">
+                            <h2>Certificates</h2>
+                        </div>
+
+                        <div class="portfolio row">
+                            <div class="col-md-12">
+                                <div class="portfolio-data">
+                                    <a href="">[Certificate Title]</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="skills">
+                        <div class="skills-title">
+                            <h2>Languages</h2>
+                        </div>
+
+                        <div class="portfolio row">
+                            <div class="col-md-12">
+                                <div class="portfolio-data">
+                                    <div class="col-md-4 col-sm-4">
+                                        <strong>[English]</strong>
+                                    </div>
+
+                                    <div class="col-md-4 col-sm-4">
+                                        <strong>[Bulgarisch]</strong>
+                                    </div>
+
+                                    <div class="col-md-4 col-sm-4">
+                                        <strong>[German]</strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="skills">
+                        <div class="skills-title">
+                            <h2>Education</h2>
+                        </div>
+
+                        <div class="portfolio row">
+                            <div class="col-md-12">
+                                <div class="portfolio-data">
+                                    <strong>[Education Centre Title]</strong>
+                                    <small>([location])</small>
+                                    <br>
+                                    <small>[date]</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+
+            $('.green-bar').each(function () {
+                var $this = $(this),
+                    $parentOfThis = $this.parent(),
+                    $elementBeforeParent = $parentOfThis.prev(),
+                    $currentDataWidth = $this.attr('data-width');
+                $elementBeforeParent.val($currentDataWidth);
+            });
+        });
+    </script></div>
